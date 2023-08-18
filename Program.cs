54c0d400d3207d616f556568eca029f413f45063
Filip Działa <fi.dziala@gmail.com>
2023-08-18 18:03:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IamprogrammerVI
+{
+    class Program 
+    {
+        // 
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student();
+            Student s2 = new Student();
+            Student s3 = new Student();
+
+            s1.Name = "Karol";
+            s1.Age = 26;
+            s1.FinalScore = 95;
+            s1.Hello();
+
+            s2.Name = "Kuba";
+            s2.Age = 28;
+            s2.FinalScore = 90;
+
+            s3.Name = "Artur";
+            s3.Age = 26;
+            s3.FinalScore = 90;
+
+           
+            Console.WriteLine(s2.Name);
+            Console.WriteLine(s3.Name);
+
+
+        }
+
+
+
+    }
+
+ 
+}
+
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IamprogrammerVI
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+        public double FinalScore;
+
+        public void Hello()
+        {
+            Console.WriteLine("Hello from {0}", Name);
+        }
+    }
+}
